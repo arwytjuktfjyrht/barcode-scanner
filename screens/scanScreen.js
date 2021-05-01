7a0155ca08ca49cabd169a1167ed8e9c2dfd2714
bobbibbc <bobbibbc@gmail.com>
2021-05-01 11:55:42 -0500

    barcode scanner

---
+++ screens/scanScreen.js
@@ -0,0 +1,72 @@
+import React from "react";
+import * as Permissions from 'expo-permissions';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { Text, View, TouchableOpacity, StyleSheet, Image } from 'react-native';
+
+export default class ScanScreen extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            hasCameraPermissions: null,
+            scanned: false,
+            scannedData: '',
+            buttonState: 'normal'
+        }
+    }
+    getCameraPermissions = async () => {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA)
+        this.setState({
+            hasCameraPermissions: status === 'granted',
+            buttonState: 'clicked',
+        })
+    }
+    handleBarCodeScanner = async ({type, data}) => {
+        this.setState({
+            scanned: true,
+            scannedData: 'data',
+            buttonState: 'normal',
+        });
+    }
+    render() {
+        const scanned = this.state.scanned;
+        const buttonState = this.state.buttonState;
+        const hasCameraPermissions = this.state.hasCameraPermissions;
+        if (buttonState === 'clicked' && hasCameraPermissions) {
+          return (
+            <BarCodeScanner
+              onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+              style={StyleSheet.absoluteFillObject}
+            />
+          );
+        } else if (buttonState === 'normal') {
+          return (
+            <View style={styles.container}>
+                <View>
+                    <Image source = {require("../assets/220px-Barcode-scanner.jpg")} style={{width:200, height:200}}/>
+                    <Text style = {{textAlign: 'center', fontSize: 30}}>
+                        BarCodeScanner
+                    </Text>
+                </View>
+              <Text style={styles.displayText}>
+                {hasCameraPermissions === true
+                  ? this.state.scannedData
+                  : 'Request camera permission'}
+              </Text>
+              <TouchableOpacity
+                style={styles.scanButton}
+                onPress={this.getCameraPermissions}>
+                <Text style={styles.buttonText}>Scan QR Code</Text>
+              </TouchableOpacity>
+            </View>
+          );
+        }
+      }
+    }
+    
+    const styles = StyleSheet.create({
+      container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+      displayText: { fontSize: 15, textDecorationLine: 'underline' },
+      scanButton: { backgroundColor: '#2196F3', padding: 10, margin: 10 },
+      buttonText: { fontSize: 20 },
+    });
+    
